7765fcc5a90ace6bcad38e38c802b0c2838a7e1f
leonardo2204 <ferrari2204_5@hotmail.com>
2018-02-02 20:05:39 -0200

    add android back button support

--- boilerplate/App/Navigation/ReduxNavigation.js
+++ boilerplate/App/Navigation/ReduxNavigation.js
@@ -1,17 +1,38 @@
 import React from 'react'
-import * as ReactNavigation from 'react-navigation'
+import { BackHandler, Platform } from 'react-native'
 import { connect } from 'react-redux'
+import { addNavigationHelpers } from 'react-navigation'
 import AppNavigation from './AppNavigation'
 
 // here is our redux-aware smart component
-function ReduxNavigation (props) {
-  const { dispatch, nav } = props
-  const navigation = ReactNavigation.addNavigationHelpers({
-    dispatch,
-    state: nav
-  })
 
-  return <AppNavigation navigation={navigation} />
+class ReduxNavigation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'ios') return
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      const { dispatch, navigation, nav } = this.props;
+      //change to whatever is your first screen, otherwise unpredictable results may occur 
+      if (nav.routes.length === 1 && (nav.routes[0].routeName === 'LaunchScreen')) {
+        return false;
+      }
+      // if (shouldCloseApp(nav)) return false
+      dispatch({ type: 'Navigation/BACK' });
+      return true;
+    })
+  }
+
+  componentWillUnmount() {
+    if (Platform.OS === 'ios') return
+    BackHandler.removeEventListener('hardwareBackPress');
+  }
+
+  render() {
+    return <AppNavigation navigation={addNavigationHelpers({ dispatch: this.props.dispatch, state: this.props.nav })} />
+  }
 }
 
 const mapStateToProps = state => ({ nav: state.nav })
