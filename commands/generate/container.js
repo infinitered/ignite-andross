de1a916f35519c0d0f80280e4602e9117a9b9c22
waqas <>
2020-05-16 08:38:40 +0500

    Removed deprecated code.

--- commands/generate/container.js
+++ commands/generate/container.js
@@ -30,34 +30,6 @@ module.exports = {
     ]
 
     await ignite.copyBatch(toolbox, jobs, props)
-
-    // if using `react-navigation` go the extra step
-    // and insert the container into the nav router
-    if (config.navigation === 'react-navigation') {
-      const containerName = name
-      const appNavFilePath = `${process.cwd()}/App/Navigation/AppNavigation.js`
-      const importToAdd = `import ${containerName} from '../Containers/${containerName}'`
-      const routeToAdd = `  ${containerName}: { screen: ${containerName} },`
-
-      if (!filesystem.exists(appNavFilePath)) {
-        const msg = `No '${appNavFilePath}' file found.  Can't insert container.`
-        print.error(msg)
-        process.exit(1)
-      }
-
-      // insert container import
-      ignite.patchInFile(appNavFilePath, {
-        after: patterns[patterns.constants.PATTERN_IMPORTS],
-        insert: importToAdd
-      })
-
-      // insert container route
-      ignite.patchInFile(appNavFilePath, {
-        after: patterns[patterns.constants.PATTERN_ROUTES],
-        insert: routeToAdd
-      })
-    } else {
-      print.info('Container created, manually add it to your navigation')
-    }
+    print.info('Container created, manually add it to your navigation')
   }
 }
--- commands/generate/list.js
+++ commands/generate/list.js
@@ -1,6 +1,6 @@
 module.exports = {
   alias: ['ls', 'listview'],
-  description: 'Generates a screen with a ListView/Flatlist/SectionList + walkthrough.',
+  description: 'Generates a screen with a Flatlist/SectionList + walkthrough.',
   run: async function(toolbox) {
     const patterns = require('../../lib/patterns')
     // grab some features
@@ -18,10 +18,6 @@ module.exports = {
     const name = pascalCase(parameters.first)
     const props = { name }
 
-    // which type of list in code
-    const typeCodeMessage = 'What coding style do you want for your list?'
-    const typeCodeChoices = ['Flatlist (new)', 'Listview (classic)']
-
     // which type of layout?
     const typeMessage = 'What kind of List would you like to generate?'
     const typeChoices = ['Row', 'Grid']
@@ -31,22 +27,9 @@ module.exports = {
     const typeDataChoices = ['Single', 'Sectioned']
 
     // Check for parameters to bypass questions
-    let typeCode = parameters.options.codeType
+    let typeCode = 'flatlist'
     let type = parameters.options.type
     let dataType = parameters.options.dataType
-
-    // only prompt if type is not defined
-    if (!typeCode) {
-      // as question 1
-      const codeAnswers = await toolbox.prompt.ask({
-        name: 'type',
-        type: 'list',
-        message: typeCodeMessage,
-        choices: typeCodeChoices
-      })
-      typeCode = codeAnswers.type[0] === typeCodeChoices[0] ? 'flatlist' : 'listview'
-      
-    }
 
     if (!type) {
       // ask question 2
@@ -56,9 +39,9 @@ module.exports = {
         message: typeMessage,
         choices: typeChoices
       })
-     
+
       type = answers.type[0] === typeDataChoices[0] ? 'row' : 'grid'
-     
+
     }
 
     if (!dataType) {
@@ -77,26 +60,14 @@ module.exports = {
     let componentTemplate =
       dataType.toLowerCase() === 'sectioned' ? typeCode + '-sections' : typeCode
     let styleTemplate = ''
-    // Different logic depending on code types
-    if (typeCode === 'flatlist') {
-      /*
-       * The following mess is because FlatList supports numColumns
-       * where SectionList does not.
-       */
-      if (type.toLowerCase() === 'grid' && dataType.toLowerCase() === 'sectioned') {
-        // grid + section means we need wrap
-        styleTemplate = 'listview-grid-style'
-      } else if (type.toLowerCase() === 'grid') {
-        componentTemplate = componentTemplate + '-grid'
-        // grid + single = no wrap, use columns
-        styleTemplate = 'flatlist-grid-style'
-      } else {
-        // no grids, flatlist basic
-        styleTemplate = 'listview-style'
-      }
-    } else {
-      // listview builder
-      styleTemplate = type.toLowerCase() === 'grid' ? 'listview-grid-style' : 'listview-style'
+    /*
+     * The following mess is because FlatList supports numColumns
+     * where SectionList does not.
+     */
+    if (type.toLowerCase() === 'grid') {
+      componentTemplate = componentTemplate + '-grid'
+      // grid + single = no wrap, use columns
+      styleTemplate = 'flatlist-grid-style'
     }
 
     const jobs = [
@@ -111,34 +82,6 @@ module.exports = {
     ]
 
     await ignite.copyBatch(toolbox, jobs, props)
-
-    // if using `react-navigation` go the extra step
-    // and insert the screen into the nav router
-    if (config.navigation === 'react-navigation') {
-      const screenName = `${name}`
-      const appNavFilePath = `${process.cwd()}/App/Navigation/AppNavigation.js`
-      const importToAdd = `import ${screenName} from '../Containers/${screenName}'`
-      const routeToAdd = `  ${screenName}: { screen: ${screenName} },`
-
-      if (!filesystem.exists(appNavFilePath)) {
-        const msg = `No '${appNavFilePath}' file found.  Can't insert list screen.`
-        print.error(msg)
-        process.exit(1)
-      }
-
-      // insert list screen import
-      ignite.patchInFile(appNavFilePath, {
-        after: patterns[patterns.constants.PATTERN_IMPORTS],
-        insert: importToAdd
-      })
-
-      // insert list screen route
-      ignite.patchInFile(appNavFilePath, {
-        after: patterns[patterns.constants.PATTERN_ROUTES],
-        insert: routeToAdd
-      })
-    } else {
-      print.info('List screen created, manually add it to your navigation')
-    }
+    print.info('List screen created, manually add it to your navigation')
   }
 }
